a645603069a96a22f019f7854bae40fa21611280
Bárbara Müller <105740844+barbaraakk@users.noreply.github.com>
2023-05-04 19:30:13 -0300

    Delete P0ST-Games-main directory

--- P0ST-Games-main/executar_teste.py
+++
@@ -1,6 +0,0 @@
-from geral.config import *
-from TestarJogo import *
-
-# inserindo a aplicação em um contexto :-/
-with app.app_context():
-    TestarJogo.run()
--- P0ST-Games-main/rotas/grupo.py
+++
@@ -1,6 +0,0 @@
-from geral.config import *
-from modelo.jogo import *
-
-@app.route("/grupo")
-def grupo():
-    return render_template('grupo.html')
