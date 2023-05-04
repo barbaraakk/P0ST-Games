6a8e8d7d0cd7a2c5a6e1eef378b4659c8a30f9dd
Bugmenn <125574692+Bugmenn@users.noreply.github.com>
2023-05-04 16:22:11 -0300

    Add files via upload

--- P0ST-Games-main/executar_teste.py
+++ P0ST-Games-main/executar_teste.py
@@ -1,6 +1,6 @@
 from geral.config import *
-from TestarJogo import *
+from TestarJogo import run
 
 # inserindo a aplicação em um contexto :-/
 with app.app_context():
-    TestarJogo.run()
+    run()
